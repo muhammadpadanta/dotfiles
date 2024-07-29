6e2abf2d19062398e427f8537a53156ba6abe6f8
muhammadpadanta <dantagaul22@gmail.com>
2024-07-29 12:17:10 +0700

    Added mise and new nvim config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -88,11 +88,20 @@ function cd
     end
 end
 
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+
 # Initialize #
 starship init fish | source
 zoxide init fish | source
 source ~/.asdf/asdf.fish
 ~/.local/bin/mise activate fish | source
+#~/linuxbrew/.linuxbrew/bin/mise activate fish | source
 # set -Ux PYENV_ROOT $HOME/.pyenv
 # set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 # pyenv init - | source
